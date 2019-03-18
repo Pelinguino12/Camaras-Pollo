d781fd1c2dfb7e0600782838625f17b15eb64b57
Pelinguino12 <pelinguino@gmail.com>
2019-03-18 17:36:54 +0100

    Modificación 1

---
+++ Assets/Scripts/Pollo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollo : MonoBehaviour {
+	
+	[SerializeField] int force = 100;
+	private Rigidbody rb;
+	bool tocoSuelo = true;
+	[SerializeField] Transform posicionPies;
+	float distanciaDeteccion = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	bool EstoyEnElSuelo ()
+	{
+		int layer = LayerMask.GetMask ("Terreno");
+		tocoSuelo = Physics.CheckSphere (posicionPies.position, distanciaDeteccion, layer);
+		return tocoSuelo;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (EstoyEnElSuelo ())
+		{
+			if (Input.GetKeyDown("up"))
+			{
+				rb.AddForce(new Vector3(0, 1, 1) * force);
+			}
+			if (Input.GetKeyDown("down"))
+			{
+				rb.AddForce(new Vector3(0, 1, -1) * force);
+			}
+			if (Input.GetKeyDown("left"))
+			{
+				rb.AddForce(new Vector3(-1, 1, 0) * force);
+			}
+			if (Input.GetKeyDown("right"))
+			{
+				rb.AddForce(new Vector3(1, 1, 0) * force);
+			}
+		}
+	}
+}
